00d822d2ba0f75010497a2f841ca6ce55aa4d476
THowland1 <tomhowland9@gmail.com>
2023-03-18 16:42:53 +0000

    ✏️ Add "Look at this graph"

--- src/routes/HomePage.svelte
+++ src/routes/HomePage.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
 	import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 	import { onMount } from 'svelte';
-	const ffmpeg = createFFmpeg({ log: true });
+	let ffmpeg = createFFmpeg({ log: true });
 
-	let ready = false;
 	let video: File | null = null;
+	let video2: File | null = null;
 	let gif: string | null = null;
 
 	onMount(() => {
-		ready = true;
 		load();
+		fetch('https://res.cloudinary.com/desh5zjme/video/upload/v1679157018/look-at-this-graph.mov')
+			.then((e) => e.blob())
+			.then((blob) => new File([blob], 'video', { type: blob.type }));
 	});
 
 	const load = async () => {
 		await ffmpeg.load();
-		ready = true;
+		ffmpeg = ffmpeg;
 	};
 
 	const convertToGif = async () => {
 		// Write the file to memory
 		ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
-
 		// Run the FFMpeg command
 		await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
 
@@ -33,11 +34,13 @@
 	};
 </script>
 
-<p class="text-xl">{ready ? 'READY' : 'NOT READY'}</p>
+<p class="text-xl">{ffmpeg.isLoaded() ? 'READY' : 'NOT READY'}</p>
 
 {#if video}
 	<video controls width="250" src={URL.createObjectURL(video)}> <track kind="captions" /></video>
 {/if}
+
+<video controls width="450" src="/look-at-this-graph.mov"> <track kind="captions" /></video>
 
 <input type="file" on:change={(e) => (video = e.currentTarget?.files?.item(0) ?? null)} />
 
